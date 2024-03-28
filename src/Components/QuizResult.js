0667b7682b20c56d85c3b7a994689cee5b5a907f
virani-077 <vishal07.360design@gmail.com>
2024-03-28 12:34:52 +0530

    quiz

---
+++ src/Components/QuizResult.js
@@ -0,0 +1,19 @@
+// QuizResult.js
+import React from "react";
+
+const QuizResult = (props) => {
+  return (
+    <>
+      <div className="show-score">
+        Your Score:{props.score}
+        <br />
+        Total Score:{props.totalScore}
+      </div>
+      <button id="next-button" onClick={props.tryAgain}>
+        Try Again
+      </button>
+    </>
+  );
+};
+
+export default QuizResult;
