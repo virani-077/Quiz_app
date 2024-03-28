a86a41eb022658b29a9cf44edbd024956d4982c5
virani-077 <vishal07.360design@gmail.com>
2024-03-28 16:11:19 +0530

    quiz

--- src/Components/QuizData.js
+++ src/Components/QuizData.js
@@ -25,8 +25,8 @@ export const QuizData = [
         answer: 1
     },
     {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
-        answer: 1
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
     },
 ];
